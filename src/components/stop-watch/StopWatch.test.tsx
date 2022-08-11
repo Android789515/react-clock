01c27f5230a5a5b4fa414e54fbf1c8b308924bd9
Android789515 <derflug@mailfence.com>
2022-08-11 18:52:38 -0500

    Formatted StopWatch.test.tsx

--- src/components/stop-watch/StopWatch.test.tsx
+++ src/components/stop-watch/StopWatch.test.tsx
@@ -1,7 +1,6 @@
-import { screen, render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { AriaRoles } from '../../types/ariaTypes';
-
 
 import { ThemeContextProvider } from '../../theme-context/themeContext';
 import StopWatch from './StopWatch';
