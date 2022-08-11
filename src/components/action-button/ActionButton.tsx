8fbc7839dbbd550ca569e3bd5ca98f6a80ce8505
Android789515 <derflug@mailfence.com>
2022-08-11 18:25:51 -0500

    Formatted ActionButton.tsx

--- src/components/action-button/ActionButton.tsx
+++ src/components/action-button/ActionButton.tsx
@@ -4,19 +4,24 @@ import type { AriaControls } from '../../types/ariaTypes';
 
 type Text = string;
 type CSS_Styles = string;
+
 interface Props {
     actionName: Text;
     styles?: CSS_Styles;
-    ariaControls: AriaControls
+    ariaControls: AriaControls;
     onClick: MouseEventHandler;
 }
 
 const ActionButton = ({ actionName, styles, ariaControls, onClick }: Props) => {
     return (
-        <button className={styles} onClick={onClick} aria-controls={ariaControls}>
+        <button
+            aria-controls={ariaControls}
+            className={styles}
+            onClick={onClick}
+        >
             {actionName}
         </button>
-    )
+    );
 };
 
 export default ActionButton;
