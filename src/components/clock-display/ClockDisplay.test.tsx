505d9100bf1d07d08d00033837a8389c8fb5674a
Android789515 <derflug@mailfence.com>
2022-08-11 18:36:16 -0500

    Added a new test to ClockDisplay.test.tsx to test if it displays milliseconds and displays it properly along with the rest of the derived time.

--- src/components/clock-display/ClockDisplay.test.tsx
+++ src/components/clock-display/ClockDisplay.test.tsx
@@ -21,4 +21,22 @@ describe('ClockDisplay', () => {
 
         expect(actualDisplayResult).toEqual(expectedDisplayResult);
     });
+
+    it('Takes an optional prop instructing whether to display the milliseconds', () => {
+        const millisecondsToTest = 9_857_655;
+
+        render(
+            <ClockDisplay
+                timeInMilliseconds={millisecondsToTest}
+                showMilliseconds={true}
+            />
+        );
+
+        const DisplayElement = screen.getByRole(AriaRoles.timer);
+
+        const expectedDisplayResult = '02:44:17.55';
+        const actualDisplayResult = DisplayElement.textContent;
+
+        expect(actualDisplayResult).toEqual(expectedDisplayResult);
+    });
 });
