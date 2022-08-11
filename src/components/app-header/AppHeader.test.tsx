1fd7a6ce87431af5a951e1fb99c6196b5f8dd1d0
Android789515 <derflug@mailfence.com>
2022-08-11 18:31:20 -0500

    Wrapped the AppHeader component in the ThemeContextProvider, so it can function properly in testing.  Imported the AriaRoles enum and replaced string literal with the respective enum value.

--- src/components/app-header/AppHeader.test.tsx
+++ src/components/app-header/AppHeader.test.tsx
@@ -1,12 +1,19 @@
 import { screen, render } from '@testing-library/react';
 
+import { AriaRoles } from '../../types/ariaTypes';
+
+import { ThemeContextProvider } from '../../theme-context/themeContext';
 import AppHeader from './AppHeader';
 
 describe('AppHeader', () => {
     it('Renders a ThemeSwitch component', () => {
-        render(<AppHeader />);
+        render(
+            <ThemeContextProvider>
+                <AppHeader />
+            </ThemeContextProvider>
+        );
 
-        const ThemeSwitchComponent = screen.getByRole('button');
+        const ThemeSwitchComponent = screen.getByRole(AriaRoles.button);
 
         expect(ThemeSwitchComponent).toBeInTheDocument();
     });
