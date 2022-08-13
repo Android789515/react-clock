c4ff7f9f1d633f29a116e53373814af1a7b29113
Android789515 <derflug@mailfence.com>
2022-08-13 15:06:02 -0500

    Created the Timer component, a new feature component which will count time from a specified time that can be set.

---
+++ src/components/timer/Timer.tsx
@@ -0,0 +1,22 @@
+import styles from './Timer.module.scss';
+
+import ClockDisplay from '../clock-display/ClockDisplay';
+import TimerBar from './timer-bar/TimerBar';
+
+const Timer = () => {
+    return (
+        <div>
+            <ClockDisplay
+                timeInMilliseconds={0}
+            />
+
+            <TimerBar
+                isActive={false}
+                currentTimeInSeconds={0}
+                totalTimeInSeconds={1000}
+            />
+        </div>
+    );
+};
+
+export default Timer;
