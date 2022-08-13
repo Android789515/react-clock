9ce12f786774c1b043a56f96dce0509453263879
Android789515 <derflug@mailfence.com>
2022-08-13 17:11:37 -0500

    Created a new, almost identical component from the StopWatchButtons, so it can replace it and be re-usable

---
+++ src/components/clock-action-buttons/ClockActionButtons.tsx
@@ -0,0 +1,64 @@
+import { useContext } from 'react';
+
+import { AriaRoles } from '../../types/ariaTypes';
+import { clockDisplayID } from '../clock-display/ClockDisplay';
+import { themeContext } from '../../theme-context/themeContext';
+
+import styles from './StopWatchButtons.module.scss';
+
+import ActionButton from '../action-button/ActionButton';
+
+type StopWatchAction = () => void;
+
+interface Props {
+    isClockStarted?: boolean;
+    startCounting: StopWatchAction;
+    stopCounting: StopWatchAction;
+    resetTime: StopWatchAction;
+}
+
+const ClockActionButtons = ({ isClockStarted, startCounting, stopCounting, resetTime }: Props) => {
+    const { getTheme } = useContext(themeContext);
+
+    return (
+        <div
+            title='Stop Watch Buttons'
+            className={`
+                ${styles.stopWatchButtons}
+                ${styles[getTheme()]}
+            `}
+            role={AriaRoles.toolbar}
+        >
+            {isClockStarted
+                ? (
+                    <ActionButton
+                        actionName='Lap'
+                        ariaControls={clockDisplayID}
+                        onClick={() => {}}
+                    />
+                )
+                : (
+                    <ActionButton
+                        actionName='Start'
+                        ariaControls={clockDisplayID}
+                        onClick={startCounting}
+                    />
+                )
+            }
+
+            <ActionButton
+                actionName='Stop'
+                ariaControls={clockDisplayID}
+                onClick={stopCounting}
+            />
+
+            <ActionButton
+                actionName='Reset'
+                ariaControls={clockDisplayID}
+                onClick={resetTime}
+            />
+        </div>
+    );
+};
+
+export default ClockActionButtons;
