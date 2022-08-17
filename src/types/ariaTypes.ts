a623c2d464752ba998e704afcb8d08ef1d97c09e
Android789515 <derflug@mailfence.com>
2022-08-16 20:36:32 -0500

    Created more aria roles for the AriaRoles enum.  Also created two new enums for heading levels and input types.

--- src/types/ariaTypes.ts
+++ src/types/ariaTypes.ts
@@ -2,10 +2,27 @@ import type { HTML_ID } from './HTML_Types';
 
 export enum AriaRoles {
     button = 'button',
+    heading = 'heading',
+    input = 'input',
     main = 'main',
     progressbar = 'progressbar',
     timer = 'timer',
     toolbar = 'toolbar'
+}
+
+// First heading level is defined and
+// the rest increment from that
+export enum HeadingLevels {
+    h1 = 1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6
+}
+
+export enum InputTypes {
+    number = 'number'
 }
 
 export type AriaControls = HTML_ID;
