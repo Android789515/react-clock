f7dc440e46f2706eb9b37ab145b54b9886ed6075
Android789515 <derflug@mailfence.com>
2022-08-26 11:23:01 -0500

    Removed unused HeadingLevels enum and removed unused heading role from AriaRoles enum.
    Refactored the input role on the AriaRoles enum.
    
    Refactored the InputTypes enum, removing the unused number type and
    replacing it with text type.

--- src/types/ariaTypes.ts
+++ src/types/ariaTypes.ts
@@ -2,27 +2,15 @@ import type { HTML_ID } from './HTML_Types';
 
 export enum AriaRoles {
     button = 'button',
-    heading = 'heading',
-    input = 'input',
     main = 'main',
     progressbar = 'progressbar',
+    textInput = 'textbox',
     timer = 'timer',
-    toolbar = 'toolbar'
-}
-
-// First heading level is defined and
-// the rest increment from that
-export enum HeadingLevels {
-    h1 = 1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6
+    toolbar = 'toolbar',
 }
 
 export enum InputTypes {
-    number = 'number'
+    text = 'text',
 }
 
 export type AriaControls = HTML_ID;
