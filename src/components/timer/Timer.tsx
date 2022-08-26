aeb3548b2dd13187928b831ac5d2973f54297abe
Android789515 <derflug@mailfence.com>
2022-08-26 14:58:43 -0500

    Added functionality and state to the Timer component.
    Added two parts of state, one for the time to count from, which the user
    can update and one for decrementing the time, that the timer uses.
    
    Added a useEffect hook to set the counter time when the user updates the
    time to count from.
    
    Added a useEffect hook to prevent negative time when counting down.
    
    Added state to keep track of if the clock is started.

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -1,3 +1,8 @@
+import { useEffect, useState } from 'react';
+
+import type { TimeInSeconds } from '../../types/timeTypes';
+import useClock from '../../hooks/clock/useClock';
+
 import styles from './Timer.module.scss';
 
 import ClockDisplay from '../clock-display/ClockDisplay';
@@ -5,6 +10,50 @@ import TimerProgressBar from './timer-progress-bar/TimerProgressBar';
 import ClockActionButtons from '../clock-action-buttons/ClockActionButtons';
 
 const Timer = () => {
+    // What the user can set.
+    const [ timeToCountFrom, updateTimeToCountFrom ] = useState<TimeInSeconds>(0);
+
+    // What the clock uses.
+    // Less confusing to call it counterTime than timerTime.
+    const [ counterTime, updateCounterTime ] = useState<TimeInSeconds>(timeToCountFrom)
+
+    const incrementCounterTime = () => updateCounterTime(prevTime => {
+        if (prevTime) {
+            return prevTime - 1;
+        } else {
+            return prevTime;
+        }
+    });
+    const resetCounterTime = () => updateCounterTime(timeToCountFrom);
+    // Sets the counter time if the time to count
+    // from is updated by the user.
+    useEffect(resetCounterTime, [timeToCountFrom]);
+
+    const [ isClockStarted, setIsClockStarted ] = useState(false);
+    const { startClock, stopClock } = useClock(incrementCounterTime);
+
+    const startTimer = () => {
+        startClock();
+        setIsClockStarted(true);
+    };
+
+    const stopTimer = () => {
+        stopClock();
+        setIsClockStarted(false);
+    };
+
+    const resetTimer = () => {
+        stopTimer();
+        resetCounterTime();
+    };
+
+    const preventNegativeTime = () => {
+        if (isClockStarted && counterTime === 0) {
+            updateCounterTime(0);
+            stopTimer();
+        }
+    };
+    useEffect(preventNegativeTime, [isClockStarted]);
 
     return (
         <div
