0dd0f71799485e1383d869c9e5e5575162055c90
Android789515 <derflug@mailfence.com>
2022-10-08 14:45:41 -0500

    Added a test file for the Timer component with two tests to check that it renders its child components.

---
+++ src/components/timer/Timer.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+
+import { editableClockDisplayOverlayLabel } from './editable-clock-display-overlay/EditableClockDisplayOverlay';
+import { timerProgressBarLabel } from './timer-progress-bar/TimerProgressBar';
+
+import Timer from './Timer';
+
+describe('Timer', () => {
+    it('Renders an EditableClockDisplayOverlay component', () => {
+        render(<Timer />);
+
+        const EditableClockDisplayOverlay = screen.getByLabelText(editableClockDisplayOverlayLabel);
+        expect(EditableClockDisplayOverlay).toBeInTheDocument();
+    });
+
+    it('Renders a TimerProgressBar component', () => {
+        render(<Timer />);
+
+        const TimerProgressBar = screen.getByLabelText(timerProgressBarLabel);
+        expect(TimerProgressBar).toBeInTheDocument();
+    });
+});
