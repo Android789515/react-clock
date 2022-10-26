f65fed75d6947ca996802b00ff1b80a40c488864
Android789515 <derflug@mailfence.com>
2022-10-26 17:03:09 -0500

    Refactored ClockDisplay component test file.
    Refactored the tests for the ClockDisplay component to test the features
    a user can see and interact with.

--- src/components/clock-display/ClockDisplay.test.tsx
+++ src/components/clock-display/ClockDisplay.test.tsx
@@ -1,43 +1,57 @@
-import { screen, render, fireEvent } from '@testing-library/react';
+import { screen, fireEvent, render } from '@testing-library/react';
 
 import { AriaRoles } from '../../types/ariaTypes';
+import { TimeInMilliseconds } from '../../types/timeTypes';
+import { toMilliseconds } from '../../utils/timeConversionUtils';
 
 import ClockDisplay from './ClockDisplay';
 
 describe('ClockDisplay', () => {
+    it('Displays a formatted time in milliseconds ' +
+        'as hours:minutes:seconds.milliseconds (milliseconds are optional)', () => {
 
-    it('Takes an optional prop instructing whether to display the milliseconds', () => {
         const millisecondsToTest = 9_857_655;
-
         render(
             <ClockDisplay
                 disabled={true}
-                showMilliseconds={true}
                 timeInMilliseconds={millisecondsToTest}
-                updateTimeInSeconds={() => {}}
             />
         );
 
-        const DisplayElement = screen.getByRole(AriaRoles.timer);
-        const expectedDisplayResult = '02:44:17.655';
-        expect(DisplayElement).toHaveDisplayValue(expectedDisplayResult);
+        const Component = screen.getByRole(AriaRoles.timer);
+        const expectedDisplay = '02:44:17';
+
+        expect(Component).toHaveDisplayValue(expectedDisplay);
     });
 
-    it('Updates its displayed time when focused and typed in', async () => {
-        render(
+    it('Can change its displayed time by updating the ' +
+        'time passed to it when input is enabled', () => {
+
+        let time: TimeInMilliseconds = 0;
+        const { rerender } = render(
             <ClockDisplay
-                disabled={true}
-                timeInMilliseconds={0}
-                updateTimeInSeconds={() => {}}
+                disabled={false}
+                timeInMilliseconds={time}
+                // User enters time in seconds, and it must
+                // be converted to ms to be re-passed as a prop.
+                setTime={newTime => time = toMilliseconds(newTime)}
             />
         );
 
-        const DisplayElement = screen.getByRole(AriaRoles.timer);
+        const Component = screen.getByRole(AriaRoles.timer);
+        fireEvent.change(Component, { target: { value: '12' } });
+        rerender(
+            <ClockDisplay
+                disabled={false}
+                timeInMilliseconds={time}
+                // User enters time in seconds, and it must
+                // be converted to ms to be re-passed as a prop.
+                setTime={newTime => time = toMilliseconds(newTime)}
+            />
+        );
 
-        const updatedSeconds = 12;
-        fireEvent.change(DisplayElement, { target: { value: updatedSeconds } });
-
-        const expectedDisplay = '00:00:12';
-        expect(DisplayElement).toHaveDisplayValue(expectedDisplay);
+        const UpdatedComponent = screen.getByRole(AriaRoles.timer);
+        const expectedDisplayAfterChange = '00:00:12';
+        expect(UpdatedComponent).toHaveDisplayValue(expectedDisplayAfterChange);
     });
 });
