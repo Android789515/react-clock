cedfa5b65c148da26a2cef313ff37a3192e71cbb
Android789515 <derflug@mailfence.com>
2022-10-27 19:37:42 -0500

    ClockDisplay refactor.
    Refactored the ClockDisplay component to improve experience when entering time into the input field.  Time can now be entered just as on a microwave, entering 1-2-3 will display 00:01:23 on the display; when backspaced, digits are removed properly, and they reorder themselves.

--- src/components/clock-display/ClockDisplay.tsx
+++ src/components/clock-display/ClockDisplay.tsx
@@ -2,8 +2,8 @@ import { ChangeEvent } from 'react';
 
 import { AriaRoles, InputTypes } from '../../types/ariaTypes';
 import type { TimeInMilliseconds, TimeInSeconds } from '../../types/timeTypes';
-import { removeCharacter } from '../../utils/stringUtils';
-import { formatTime } from '../../utils/timeConversionUtils';
+import { formatTime, getTotalSeconds } from '../../utils/timeConversionUtils';
+import { prefix, removeCharacter, segmentString } from '../../utils/stringUtils';
 
 import styles from './ClockDisplay.module.scss';
 
@@ -11,30 +11,69 @@ interface Props {
     disabled: boolean;
     showMilliseconds?: boolean;
     timeInMilliseconds: TimeInMilliseconds;
-    setTime?: (time: TimeInMilliseconds | TimeInSeconds) => void;
+    setTime?: (time: TimeInSeconds) => void;
 }
 
 const ClockDisplay = ({ disabled, showMilliseconds, timeInMilliseconds, setTime }: Props) => {
-    const maxDisplayTimeLength = 6;
 
-    const setDisplayTime = ({ target }: ChangeEvent) => {
-        // Remove the colons from the updated display time.
-        const displayTime = removeCharacter(':', (target as HTMLInputElement).value);
-        // Automatically remove leading zeros.
-        const newTime = Number(displayTime);
+    const maxTimeDigits = 6;
 
-        const wasTimeSetterDefined = setTime !== undefined;
-        if (wasTimeSetterDefined) {
-            const isNewTimeTooLong = String(newTime).length > maxDisplayTimeLength;
-            const isNewTimeValidNumber = !Object.is(newTime, NaN);
+    const limitTimeDigits = (timeEntered: string) => {
+        const isTimeEnteredTooLong = timeEntered.length > maxTimeDigits;
 
-            if (!isNewTimeTooLong && isNewTimeValidNumber) {
-                setTime(newTime);
-            }
+        if (isTimeEnteredTooLong) {
+            return timeEntered.slice(0, maxTimeDigits);
+        } else {
+            return timeEntered;
         }
     };
 
-    const { hours, minutes, seconds, milliseconds } = formatTime(timeInMilliseconds);
+    const segmentTime = (parsedTime: string) => {
+        const zeroPrefixedTime = prefix(parsedTime, '0', maxTimeDigits - parsedTime.length);
+
+        return segmentString(zeroPrefixedTime, 2);
+    };
+
+    const validateTimeEntered = (timeEntered: string) => {
+        const isTimeEnteredValidNumber = !Object.is(Number(timeEntered), NaN);
+
+        if (isTimeEnteredValidNumber) {
+            return timeEntered;
+        } else {
+            const previousValidTime = timeEntered.slice(0, timeEntered.length - 1);
+            return previousValidTime;
+        }
+    };
+
+    const removeLeadingZeros = (validTimeEntered: string) => {
+        return String(Number(validTimeEntered));
+    };
+
+    const setDisplayTime = ({ target }: ChangeEvent) => {
+        const timeEntered = (target as HTMLInputElement).value;
+
+        const parsableTime = removeCharacter(':', timeEntered);
+        const validatedParsableTime = validateTimeEntered(parsableTime);
+        const parsedTime = limitTimeDigits(
+            removeLeadingZeros(validatedParsableTime)
+        );
+
+        const timeSegments = segmentTime(parsedTime).map(segment => Number(segment));
+        const [ hours, minutes, seconds ] = timeSegments;
+
+        const totalSeconds = getTotalSeconds(hours, minutes, seconds);
+
+        if (setTime) {
+            setTime(totalSeconds);
+        }
+    };
+
+    const {
+        formattedHours,
+        formattedMinutes,
+        formattedSeconds,
+        formattedMilliseconds
+    } = formatTime(timeInMilliseconds);
     return (
         <input
             className={styles.clockDisplay}
@@ -43,10 +82,10 @@ const ClockDisplay = ({ disabled, showMilliseconds, timeInMilliseconds, setTime 
             type={InputTypes.text}
             disabled={disabled}
             value={
-                hours + ':'
-                + minutes + ':'
-                + seconds
-                + (showMilliseconds ? '.' + milliseconds : '')
+                formattedHours + ':'
+                + formattedMinutes + ':'
+                + formattedSeconds
+                + (showMilliseconds ? `.${formattedMilliseconds}` : '')
             }
             onChange={setDisplayTime}
         />
