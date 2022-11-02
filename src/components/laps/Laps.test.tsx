b2f79ae3d0e2c849acd91d68c3dc3e88694352b5
Android789515 <derflug@mailfence.com>
2022-11-02 16:31:05 -0500

    Created a folder and boilerplate files for a new component, Laps.
    Laps will display laps created by the StopWatch component.

---
+++ src/components/laps/Laps.test.tsx
@@ -0,0 +1,7 @@
+import { screen, render } from '@testing-library/react';
+
+import Laps from './Laps';
+
+describe('Laps', () => {
+
+});
---
+++ src/components/laps/Laps.tsx
@@ -0,0 +1,11 @@
+import styles from './Laps.module.scss';
+
+const Laps = () => {
+    return (
+        <ul>
+
+        </ul>
+    );
+};
+
+export default Laps;
