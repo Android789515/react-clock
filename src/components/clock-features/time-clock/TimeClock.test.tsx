7d967b74892ba1c24672c54176a3ae04e92747b6
Android789515 <derflug@mailfence.com>
2022-11-06 20:35:53 -0600

    Completed first test and implemented functionality; TimeClock component.
    Created a test for displaying the current time using a ClockDisplay component and completed red-green-refactor pattern for making the test, running a failure, then implementing the component's functionality and retesting for it to pass.

--- src/components/clock-features/time-clock/TimeClock.test.tsx
+++ src/components/clock-features/time-clock/TimeClock.test.tsx
@@ -1,7 +1,25 @@
 import { screen, render } from '@testing-library/react';
 
+import { AriaRoles } from '../../../types/ariaTypes';
+
 import TimeClock from './TimeClock';
 
 describe('TimeClock', () => {
+    it('Renders a non-editable ClockDisplay with the current local time', async () => {
+        render(
+            <TimeClock />
+        );
 
+        const ClockDisplay = screen.getByRole(AriaRoles.timer);
+
+        expect(ClockDisplay).toBeInTheDocument();
+        expect(ClockDisplay).toBeDisabled();
+
+        const date = new Date();
+        expect(ClockDisplay).toHaveDisplayValue(
+            date.getHours() + ':'
+            + date.getMinutes() + ':'
+            + date.getSeconds()
+        );
+    });
 });
--- src/components/clock-features/time-clock/TimeClock.tsx
+++ src/components/clock-features/time-clock/TimeClock.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from 'react';
+
+import { TimeInMilliseconds } from '../../../types/timeTypes';
+import { toMilliseconds } from '../../../utils/timeConversionUtils';
+import useClock from '../../../independent-hooks/clock/useClock';
+
 import styles from './TimeClock.module.scss';
 
+import ClockDisplay from '../../clock-display/ClockDisplay';
+
 const TimeClock = () => {
+    const getCurrentTime = () => {
+        const currentDate = new Date();
+        const hours = currentDate.getHours();
+        const minutes = currentDate.getMinutes();
+        const seconds = currentDate.getSeconds();
+        const milliseconds = currentDate.getMilliseconds();
+
+        return (
+            (hours *  3.6e+6)
+            + (minutes * 6e+4)
+            + toMilliseconds(seconds)
+            + milliseconds
+        );
+    };
+
+    const [ currentTime, setCurrentTime ] = useState<TimeInMilliseconds>(getCurrentTime());
+
+    const { startClock, stopClock } = useClock(() => {
+        setCurrentTime(getCurrentTime());
+    });
+
+    useEffect(() => {
+        startClock();
+
+        return () => stopClock();
+    }, []);
+
     return (
         <main>
-
+            <ClockDisplay
+                disabled
+                timeInMilliseconds={currentTime}
+            />
         </main>
     );
 };
