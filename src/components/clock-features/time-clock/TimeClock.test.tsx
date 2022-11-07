5f62d6c70384a76664d9f5d216a1d844b207a765
Android789515 <derflug@mailfence.com>
2022-11-06 18:25:32 -0600

    Started boilerplate for time clock feature.
    Created a folder and boilerplate files for a new component, TimeClock, that will display the current local time.

---
+++ src/components/clock-features/time-clock/TimeClock.test.tsx
@@ -0,0 +1,7 @@
+import { screen, render } from '@testing-library/react';
+
+import TimeClock from './TimeClock';
+
+describe('TimeClock', () => {
+
+});
---
+++ src/components/clock-features/time-clock/TimeClock.tsx
@@ -0,0 +1,11 @@
+import styles from './TimeClock.module.scss';
+
+const TimeClock = () => {
+    return (
+        <main>
+
+        </main>
+    );
+};
+
+export default TimeClock;
