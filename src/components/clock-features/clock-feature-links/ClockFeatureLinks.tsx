d30b2bd11dc77c173500649d9160f4b59edf63f4
Android789515 <derflug@mailfence.com>
2022-11-11 17:44:00 -0600

    Added more boilerplate to the component files.
    Defined the HTML structure and semantics for the component's JSX.
    
    Hooked up the class for the component and added a list un-style mixin to it.

--- src/components/clock-features/clock-feature-links/ClockFeatureLinks.tsx
+++ src/components/clock-features/clock-feature-links/ClockFeatureLinks.tsx
@@ -2,7 +2,9 @@ import styles from './ClockFeatureLinks.module.scss';
 
 const ClockFeatureLinks = () => {
     return (
-        <></>
+        <ul className={styles.clockFeatureLinks}>
+
+        </ul>
     );
 };
 
