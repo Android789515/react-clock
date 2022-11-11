e21e6f3b26b215c5e7f9f174d82f96e6e8dd9abe
Android789515 <derflug@mailfence.com>
2022-11-11 17:40:44 -0600

    Created boilerplate for new component, ClockFeatureLinks.
    Created a folder for the component and boilerplate files for it.

---
+++ src/components/clock-features/clock-feature-links/ClockFeatureLinks.test.tsx
@@ -0,0 +1,7 @@
+import { screen, render } from '@testing-library/react';
+
+import ClockFeatureLinks from './ClockFeatureLinks';
+
+describe('ClockFeatureLinks', () => {
+
+});
---
+++ src/components/clock-features/clock-feature-links/ClockFeatureLinks.tsx
@@ -0,0 +1,9 @@
+import styles from './ClockFeatureLinks.module.scss';
+
+const ClockFeatureLinks = () => {
+    return (
+        <></>
+    );
+};
+
+export default ClockFeatureLinks;
