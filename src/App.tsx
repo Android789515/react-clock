24e1d036583d74892825d2a45073ee8c63203bf8
Android789515 <derflug@mailfence.com>
2022-11-12 13:01:01 -0600

    Refactored routing.
    Moved the browser router to the index file, so that the app's header can use links to navigate to the different clock features.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import { AriaRoles } from './types/ariaTypes';
 import { themeContext } from './theme-context/themeContext';
@@ -7,24 +7,6 @@ import { themeContext } from './theme-context/themeContext';
 import styles from './App.module.scss';
 
 import AppHeader from './components/app-header/AppHeader';
-import TimeClock from './components/clock-features/time-clock/TimeClock';
-import StopWatch from './components/clock-features/stop-watch/StopWatch';
-import Timer from './components/clock-features/timer/Timer';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <TimeClock />
-    },
-    {
-        path: 'stopwatch',
-        element: <StopWatch />
-    },
-    {
-        path: 'timer',
-        element: <Timer />
-    }
-]);
 
 const App = () => {
     const { isLightTheme } = useContext(themeContext);
@@ -39,7 +21,7 @@ const App = () => {
         >
             <AppHeader />
 
-            <RouterProvider router={router} />
+            <Outlet />
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { ThemeContextProvider } from './theme-context/themeContext';
 import reportWebVitals from './reportWebVitals';
 
+import { ThemeContextProvider } from './theme-context/themeContext';
 import App from './App';
 
+import TimeClock from './components/clock-features/time-clock/TimeClock';
+import StopWatch from './components/clock-features/stop-watch/StopWatch';
+import Timer from './components/clock-features/timer/Timer';
+
 import './index.css';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <TimeClock />
+            },
+            {
+                path: '/stopwatch',
+                element: <StopWatch />
+            },
+            {
+                path: '/timer',
+                element: <Timer />
+            }
+        ]
+    }
+]);
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
@@ -14,7 +40,7 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <ThemeContextProvider>
-            <App />
+            <RouterProvider router={router} />
         </ThemeContextProvider>
     </React.StrictMode>,
 );
