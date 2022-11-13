e169720c016abc6f64e105e7a7c8ddff0a02f1c5
Android789515 <derflug@mailfence.com>
2022-11-12 20:30:55 -0600

    Added more styling to the clock feature links.
    Added styling to hide the links to other pages by default and show them when the component is hovered over or focused.
    
    Placed the current page link at the beginning of the grid layout.
    
    Created logic to assign a class name to the li wrapper for the link component that corresponds to the current URL path.

--- src/components/clock-features/clock-feature-links/ClockFeatureLinks.tsx
+++ src/components/clock-features/clock-feature-links/ClockFeatureLinks.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 import timeClockIcon from './clock-feature-icons/time-clock.svg';
 import stopWatchIcon from './clock-feature-icons/stop-watch.svg';
@@ -16,23 +16,34 @@ const ClockFeatureLinks = ({ isDarkTheme }: Props) => {
         ['timer', timerIcon]
     ]);
 
-    const Links = [...routeMap.entries()].map(([link, icon], index) => (
-        <li key={index}>
-            <NavLink
-                to={link}
+    const { pathname } = useLocation();
+    const Links = [...routeMap.entries()].map(([link, icon], index) => {
+        const isCurrentFeature = pathname === '/' + link;
+
+        return (
+            <li
+                key={index}
                 className={`
+                ${isCurrentFeature ? styles.currentClockFeature : ''}
+                ${styles.clockFeatureLinkWrapper}
+            `}
+            >
+                <NavLink
+                    to={link}
+                    className={`
                     ${isDarkTheme ? styles.clockFeatureLinkDark : ''}
                     ${styles.clockFeatureLink}
                 `}
-            >
-                <img
-                    src={icon}
-                    alt={`Link to ${link}`}
-                    className={styles.clockFeatureLinkIcon}
-                />
-            </NavLink>
-        </li>
-    ));
+                >
+                    <img
+                        src={icon}
+                        alt={`Link to ${link}`}
+                        className={styles.clockFeatureLinkIcon}
+                    />
+                </NavLink>
+            </li>
+        );
+    });
 
     return (
         <nav>
