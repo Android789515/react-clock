db774f0d5275e99a5eb444f0031b9d7bddac941a
Android789515 <derflug@mailfence.com>
2022-11-20 15:16:00 -0600

    Created files for a new component.
    Created a folder and boilerplate files for a new component, SlideMenuItem, which will render a list item that can slide in a SlideMenu component.

---
+++ src/components/slide-menu/slide-menu-item/SlideMenuItem.test.tsx
@@ -0,0 +1,7 @@
+import { screen, render } from '@testing-library/react';
+
+import SlideMenuItem from './SlideMenuItem';
+
+describe('SlideMenuItem', () => {
+
+});
---
+++ src/components/slide-menu/slide-menu-item/SlideMenuItem.tsx
@@ -0,0 +1,11 @@
+import styles from './SlideMenuItem.module.scss';
+
+const SlideMenuItem = () => {
+    return (
+        <li className={styles.slideMenuItem}>
+
+        </li>
+    );
+};
+
+export default SlideMenuItem;
