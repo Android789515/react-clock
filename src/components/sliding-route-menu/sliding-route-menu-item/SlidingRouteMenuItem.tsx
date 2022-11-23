34d7aef24094be44751407a3cd2e17e7dd7e7a4d
Android789515 <derflug@mailfence.com>
2022-11-22 18:10:43 -0600

    Created a new prop, SlidingRouteMenuItem.tsx.
    Created a boolean prop for the component determining if it's the item for the active route.
    
    Set up styles for the component for managing menu item positions.

--- src/components/sliding-route-menu/sliding-route-menu-item/SlidingRouteMenuItem.tsx
+++ src/components/sliding-route-menu/sliding-route-menu-item/SlidingRouteMenuItem.tsx
@@ -5,16 +5,18 @@ import type { CSS_Class } from '../../../types/CSS_Types';
 import styles from './SlidingRouteMenuItem.module.scss';
 
 interface Props {
+    isActiveRoute: boolean;
     customClassName?: CSS_Class
     children: ReactNode
 }
 
-const SlidingRouteMenuItem = ({ customClassName, children }: Props) => {
+const SlidingRouteMenuItem = ({ isActiveRoute, customClassName, children }: Props) => {
     return (
         <li
             className={`
                 ${customClassName}
                 ${styles.slidingRouteMenuItem}
+                ${isActiveRoute ? styles.activeSlidingRouteMenuItem : ''}
             `}
         >
             {children}
