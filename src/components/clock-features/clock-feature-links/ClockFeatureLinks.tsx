44f1b9488c3ecb02032605276027744239a6f559
Android789515 <derflug@mailfence.com>
2022-11-23 14:58:08 -0600

    Refactored the ClockFeatureLinks component.
    Implemented the new SlidingRouteMenu component into the ClockFeatureLinks component's markup and removed old markup, hover state, and old link components.
    
    Removed the now unused style module for the ClockFeatureLinks component.

--- src/components/clock-features/clock-feature-links/ClockFeatureLinks.tsx
+++ src/components/clock-features/clock-feature-links/ClockFeatureLinks.tsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import useGetRouteIcons from './useGetRouteIcons';
 
-import styles from './ClockFeatureLinks.module.scss';
-
+import SlidingRouteMenu from '../../sliding-route-menu/SlidingRouteMenu';
 import ClockFeatureLink from './clock-feature-link/ClockFeatureLink';
+import { RouteURL } from '../../../types/linkTypes';
 
 interface Props {
     isDarkTheme: boolean
@@ -14,48 +14,27 @@ interface Props {
 const ClockFeatureLinks = ({ isDarkTheme }: Props) => {
     const { pathname } = useLocation();
 
-    const { getRoutesAndIcons, setActiveRouteIcon } = useGetRouteIcons();
+    const { getRoutesAndIcons, setActiveRouteIcon, getIconForRoute } = useGetRouteIcons();
 
     useEffect(() => {
         setActiveRouteIcon(pathname);
     }, [pathname])
 
-    const [ isHovered, setIsHovered ] = useState(false);
-
-    const Links = getRoutesAndIcons().map(([route, iconPath], index) => {
-        const isCurrentFeature = pathname === route || pathname === '/' + route;
-
-        return (
-            <li
-                key={index}
-                className={`
-                    ${isCurrentFeature ? styles.currentClockFeature : ''}
-                    ${styles.clockFeatureLinkWrapper}
-                    ${isDarkTheme ? styles.clockFeatureLinkWrapperDark : ''}
-                `}
-                style={{ transform: (isHovered ? 'revert' : `translateX(-${200 * index}%)`) }}
-            >
-                <ClockFeatureLink
-                    route={route}
-                    iconPath={iconPath}
-                    isDarkTheme={isDarkTheme}
-                    isActiveLink={isCurrentFeature}
-                />
-            </li>
-        );
-    });
-
+    const routes = getRoutesAndIcons().map(([ route ]) => route);
+    const isCurrentFeature = (route: RouteURL) => pathname === route || pathname === '/' + route;
     return (
         <nav>
-            <ul
-                className={styles.clockFeatureLinks}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-                onFocus={() => setIsHovered(true)}
-                onBlur={() => setIsHovered(false)}
-            >
-                {Links}
-            </ul>
+            <SlidingRouteMenu
+                routes={routes}
+                LinkComponent={(route) => (
+                    <ClockFeatureLink
+                        route={route}
+                        iconPath={getIconForRoute(route)}
+                        isDarkTheme={isDarkTheme}
+                        isActiveLink={isCurrentFeature(route)}
+                    />
+                )}
+            />
         </nav>
     );
 };
