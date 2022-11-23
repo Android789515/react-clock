4ae42235c19aa6e53850b4a9049d504f80eeafbd
Android789515 <derflug@mailfence.com>
2022-11-23 16:11:53 -0600

    Added themed variant, SlidingRouteMenuItem.tsx.
    Set up themed variant for the SlidingRouteMenuItem component, checking the theme using the theme context and applying a dark variant class accordingly.
    
    Applied a background transition to the component to keep it in sync with the other components that change their color when the theme is dark.

--- src/components/sliding-route-menu/sliding-route-menu-item/SlidingRouteMenuItem.tsx
+++ src/components/sliding-route-menu/sliding-route-menu-item/SlidingRouteMenuItem.tsx
@@ -1,6 +1,8 @@
 import type { ReactNode, CSSProperties } from 'react';
+import { useContext } from 'react';
 
 import type { CSS_Class } from '../../../types/CSS_Types';
+import { themeContext } from '../../../theme-context/themeContext';
 
 import styles from './SlidingRouteMenuItem.module.scss';
 
@@ -12,11 +14,14 @@ interface Props {
 }
 
 const SlidingRouteMenuItem = ({ isActiveRoute, customClassName, customStyle, children }: Props) => {
+    const { isDarkTheme } = useContext(themeContext);
+
     return (
         <li
             className={`
                 ${customClassName}
                 ${styles.slidingRouteMenuItem}
+                ${isDarkTheme() ? styles.activeSlidingRouteMenuItemDark : ''}
                 ${isActiveRoute ? styles.activeSlidingRouteMenuItem : ''}
             `}
             style={customStyle}
