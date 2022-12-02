420a59b26df56833fc97e376859e93813f61297d
Android789515 <derflug@mailfence.com>
2022-12-02 14:58:46 -0600

    Hooked up styles to the AlarmSetter component.
    Added markup tag and attached the class name from the component's stylesheet.

--- src/components/alarm-setter/AlarmSetter.tsx
+++ src/components/alarm-setter/AlarmSetter.tsx
@@ -2,7 +2,9 @@ import styles from './AlarmSetter.module.scss';
 
 const AlarmSetter = () => {
     return (
-        <></>
+        <div className={styles.alarmSetter}>
+
+        </div>
     );
 };
 
