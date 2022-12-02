fcc7c6b0e39704a45d2fb055af1d29a234d30e6e
Android789515 <derflug@mailfence.com>
2022-12-02 14:57:34 -0600

    Created boilerplate for a new component, AlarmSetter.
    Created a folder and boilerplate files for the component, which will set alarms.

---
+++ src/components/alarm-setter/AlarmSetter.test.tsx
@@ -0,0 +1,7 @@
+import { screen, render, renderHook } from '@testing-library/react';
+
+import AlarmSetter from './AlarmSetter';
+
+describe('AlarmSetter', () => {
+
+});
---
+++ src/components/alarm-setter/AlarmSetter.tsx
@@ -0,0 +1,9 @@
+import styles from './AlarmSetter.module.scss';
+
+const AlarmSetter = () => {
+    return (
+        <></>
+    );
+};
+
+export default AlarmSetter;
