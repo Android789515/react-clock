2af58e0718efba174714c681baf9d32bf988fa7a
Android789515 <derflug@mailfence.com>
2022-12-04 12:28:16 -0600

    Created initial test for the Alarms component.
    Created first test for rendering Alarm components for each scheduled alarm.

--- src/components/alarms/Alarms.test.tsx
+++ src/components/alarms/Alarms.test.tsx
@@ -1,7 +1,24 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, renderHook } from '@testing-library/react';
+import { useContext } from 'react';
+
+import type { FormattedTime } from '../../types/timeTypes';
+import { AriaRoles } from '../../types/ariaTypes';
+import { alarmScheduler } from '../../alarm-scheduler/alarmScheduler';
 
 import Alarms from './Alarms';
 
 describe('Alarms', () => {
+    it('Renders an Alarm component for each alarm', () => {
+        const { result } = renderHook(() => useContext(alarmScheduler));
+        const { addAlarm } = result.current;
+        const newAlarm: FormattedTime = { hours: '01', minutes: '02', seconds: '00', milliseconds: '00' };
+        addAlarm(newAlarm);
 
+        render(
+            <Alarms />
+        );
+
+        const AlarmComponents = screen.getAllByRole(AriaRoles.listItem);
+        expect(AlarmComponents).toHaveLength(1);
+    });
 });
